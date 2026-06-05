30548ad2ca4a9a0ad98c085e5c8816ee914064b8
desync_lord <mark@wonderland.ac>
2026-06-05 12:07:09 +0300

    feat(movefix): exemptions

--- src/main/java/today/vanta/client/module/impl/movement/MovementFix.java
+++ src/main/java/today/vanta/client/module/impl/movement/MovementFix.java
@@ -6,12 +6,21 @@ import today.vanta.client.event.impl.game.player.MoveFlyingEvent;
 import today.vanta.client.event.impl.game.player.MoveInputEvent;
 import today.vanta.client.module.Category;
 import today.vanta.client.module.Module;
+import today.vanta.client.module.impl.combat.KillAura;
+import today.vanta.client.module.impl.player.Scaffold;
 import today.vanta.client.processor.impl.RotationProcessor;
+import today.vanta.client.setting.impl.MultiStringSetting;
 import today.vanta.util.game.events.EventListen;
 import today.vanta.util.game.player.MovementUtil;
 import today.vanta.util.game.player.constructors.Rotation;
 
 public class MovementFix extends Module {
+    private final MultiStringSetting exemptions = MultiStringSetting.builder()
+            .name("Exemptions")
+            .value("Scaffold")
+            .values("KillAura", "Scaffold")
+            .build();
+
     public MovementFix() {
         super("MovementFix", "Fixes your yaw when in combat", Category.MOVEMENT);
         displayNames = new String[] {"MovementFix", "MovementCorrection", "CorrectMovement", "MoveFix"};
@@ -20,22 +29,43 @@ public class MovementFix extends Module {
     @EventListen
     private void onMoveInput(MoveInputEvent event) {
         if (getRotations() == null) return;
+        if (isExempted()) return;
         MovementUtil.correctMovement(event, getRotations().yaw);
     }
 
     @EventListen
     private void onStrafe(MoveFlyingEvent event) {
         if (getRotations() == null) return;
+        if (isExempted()) return;
         event.yaw = getRotations().yaw;
     }
 
     @EventListen
     private void onJump(JumpEvent event) {
         if (getRotations() == null) return;
+        if (isExempted()) return;
         event.yaw = getRotations().yaw;
     }
 
     private Rotation getRotations() {
         return RotationProcessor.getInstance().rotations;
+    }
+
+    private boolean isExempted() {
+        boolean scaffoldEnabled = exemptions.isEnabled("Scaffold")
+                && Vanta.instance.moduleStorage.getT(Scaffold.class).isEnabled();
+
+        boolean killAuraEnabled = exemptions.isEnabled("KillAura")
+                && Vanta.instance.moduleStorage.getT(KillAura.class).isEnabled();
+
+        if (scaffoldEnabled) {
+            return true;
+        }
+
+        if (killAuraEnabled) {
+            return true;
+        }
+
+        return false;
     }
 }
