456e44dd07098160bb2be0a13a13059cbee9dd0c
desync_lord <mark@wonderland.ac>
2026-06-05 10:25:39 +0300

    feat: arraylist case & space out setting

--- src/main/java/today/vanta/client/module/impl/hud/Arraylist.java
+++ src/main/java/today/vanta/client/module/impl/hud/Arraylist.java
@@ -27,6 +27,12 @@ public class Arraylist extends Module {
             .listener((setting, oldValue, newValue) -> setFont())
             .build();
 
+    private final StringSetting moduleCase = StringSetting.builder()
+            .name("Module case")
+            .value("Default")
+            .values("Default", "Lowercase", "Uppercase")
+            .build();
+
     private final NumberSetting fontSize = NumberSetting.builder()
             .name("Font size")
             .value(24)
@@ -50,7 +56,13 @@ public class Arraylist extends Module {
     background = BooleanSetting.builder()
             .name("Background")
             .value(true)
+            .build(),
+
+    spaceOut = BooleanSetting.builder()
+            .name("Space out")
+            .value(false)
             .build();
+
 
     private final NumberSetting backgroundAlpha = NumberSetting.builder()
             .name("Background alpha")
@@ -63,7 +75,7 @@ public class Arraylist extends Module {
 
     public Arraylist() {
         super("Arraylist", "Draws an arraylist of modules.", Category.HUD);
-        displayNames = new String[]{"Arraylist", "ArrayList", "Array List", "ModuleList", "Module List"};
+        displayNames = new String[]{"Arraylist", "ArrayList", "ModuleList"};
         hideFromArraylist = true;
         setEnabled(true);
     }
@@ -98,25 +110,44 @@ public class Arraylist extends Module {
 
         float y = 3;
         for (Module module : modules) {
-            String moduleName = module.displayName;
+            String name = getModuleName(module);
 
-            if (module.getSuffix() != null && module.addSuffix && suffixes.getValue()) {
-                moduleName += " §f" + module.getSuffix();
-            }
-
-            float x = event.scaledResolution.getScaledWidth() - font.getStringWidth(moduleName) - 5;
+            float x = event.scaledResolution.getScaledWidth() - font.getStringWidth(name) - 5;
 
             if (background.getValue()) {
-                RenderUtil.rectangle(x - 2, y, font.getStringWidth(moduleName) + 4, font.getFontHeight() + 5, new Color(0, 0, 0, backgroundAlpha.getValue().intValue()));
+                RenderUtil.rectangle(x - 2, y, font.getStringWidth(name) + 4, font.getFontHeight() + 5, new Color(0, 0, 0, backgroundAlpha.getValue().intValue()));
             }
 
             if (fontShadow.getValue()) {
-                font.drawStringWithShadow(moduleName, x, y + 0.7, Vanta.instance.moduleStorage.getT(Theme.class).colors[0]);
+                font.drawStringWithShadow(name, x, y + 0.7, Vanta.instance.moduleStorage.getT(Theme.class).colors[0]);
             } else {
-                font.drawString(moduleName, x, y + 0.7f, Vanta.instance.moduleStorage.getT(Theme.class).colors[0]);
+                font.drawString(name, x, y + 0.7f, Vanta.instance.moduleStorage.getT(Theme.class).colors[0]);
             }
 
             y += font.getFontHeight() + (background.getValue() ? 3 : 0) + 2;
         }
+    }
+
+    private String getModuleName(Module module) {
+        String name = module.displayName;
+
+        if (spaceOut.getValue()) {
+            name = name.replaceAll("(?<=[a-z])(?=[A-Z])", " ");
+        }
+
+        if (module.getSuffix() != null && module.addSuffix && suffixes.getValue()) {
+            name += " §f" + module.getSuffix();
+        }
+
+        switch (moduleCase.getValue()) {
+            case "Lowercase":
+                name = name.toLowerCase();
+                break;
+            case "Uppercase":
+                name = name.toUpperCase();
+                break;
+        }
+
+        return name;
     }
 }
