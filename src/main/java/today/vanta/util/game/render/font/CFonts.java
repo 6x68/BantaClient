d841bbdf906895337c3f8f28ff98e22ddf1fb5fe
luna <152616221+lunolie@users.noreply.github.com>
2026-06-08 22:04:14 +0200

    arabic looking font for the funnies

--- src/main/java/today/vanta/util/game/render/font/CFonts.java
+++ src/main/java/today/vanta/util/game/render/font/CFonts.java
@@ -16,6 +16,8 @@ public class CFonts {
     public static CFontRenderer SFPT_SEMIBOLD_20 = getFont("SFPT-Semibold", 20);
     public static CFontRenderer SFPT_MEDIUM_24 = getFont("SFPT-Medium", 24);
     public static CFontRenderer SFPT_SEMIBOLD_42 = getFont("SFPT-Semibold", 42);
+    public static CFontRenderer ARABICLOOKINGFONT = getFont("arabiclookingfont", 50);
+
 
     public static CFontRenderer getFont(String fontName, float size) {
         String key = fontName + ":" + size;
