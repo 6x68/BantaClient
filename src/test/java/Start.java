4211a780dfeefb2e786367e4b329352f6d9f09e9
desync_lord <mark@wonderland.ac>
2026-06-10 19:54:15 +0300

    fix: native folder find

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -5,7 +5,15 @@ import java.util.Arrays;
 
 public class Start {
     public static void main(String[] args) {
-        System.setProperty("org.lwjgl.librarypath", new File("./natives/" + (System.getProperty("os.name").startsWith("Windows") ? "windows" : "linux")).getAbsolutePath());
+        String os = System.getProperty("os.name").startsWith("Windows")
+                ? "windows"
+                : "linux";
+
+        System.setProperty(
+                "org.lwjgl.librarypath",
+                new File(findNatives(), os).getAbsolutePath()
+        );
+
         Main.main(concat(new String[]{"--version", "1.8.9", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
 
@@ -13,5 +21,20 @@ public class Start {
         T[] result = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, result, first.length, second.length);
         return result;
+    }
+
+    private static File findNatives() {
+        File dir = new File(System.getProperty("user.dir"));
+
+        while (dir != null) {
+            File natives = new File(dir, "natives");
+            if (natives.exists() && natives.isDirectory()) {
+                return natives;
+            }
+
+            dir = dir.getParentFile();
+        }
+
+        throw new RuntimeException("Could not find natives directory");
     }
 }
