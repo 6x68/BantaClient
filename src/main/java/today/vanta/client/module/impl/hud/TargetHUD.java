76e8f3211131331254c05943b819dc47e9cb605c
desync_lord <mark@wonderland.ac>
2026-06-12 20:22:24 +0300

    fix: targethud dragging

--- src/main/java/today/vanta/client/module/impl/hud/TargetHUD.java
+++ src/main/java/today/vanta/client/module/impl/hud/TargetHUD.java
@@ -9,7 +9,6 @@ import today.vanta.client.event.impl.game.render.DrawScreenEvent;
 import today.vanta.client.module.Category;
 import today.vanta.client.module.Module;
 import today.vanta.client.module.impl.client.Theme;
-import today.vanta.client.module.impl.hud.arraylist.BitMapRenderer;
 import today.vanta.client.processor.impl.TargetProcessor;
 import today.vanta.client.setting.Setting;
 import today.vanta.client.setting.impl.NumberSetting;
@@ -23,18 +22,15 @@ import today.vanta.util.system.math.animation.Easing;
 import java.awt.*;
 
 public class TargetHUD extends Module {
-    BitMapRenderer font = new BitMapRenderer(mc.fontRendererObj);
+    private static final Color BACKGROUND = new Color(20, 20, 20, 200);
+    private static final Color DARKER_BACKGROUND = new Color(20, 20, 20, 255);
+
     private EntityLivingBase localTarget;
     private String oldTarget;
     private boolean can;
 
-    private static final Color BACKGROUND = new Color(20, 20, 20, 200);
-    Color bgbutdarker = new Color(20, 20, 20, 255);
-
-    private static final float WIDTH = 130;
-    private static final float HEIHT = 40;
-    private float mousingY;
-    private float mousingX;
+    private float width = 130;
+    private float height = 40;
 
     private boolean dragging;
     private float dragX, dragY;
@@ -72,15 +68,12 @@ public class TargetHUD extends Module {
             return;
         }
 
-        mousingX = event.mouseX;
-        mousingY = event.mouseY;
-
         draw();
     }
 
     private void handleDragging(float mouseX, float mouseY) {
         if (Mouse.isButtonDown(0)) {
-            if (!dragging && RenderUtil.hovered(mouseX, mouseY, x.getValue().floatValue(), y.getValue().floatValue(), WIDTH, HEIHT)) {
+            if (!dragging && RenderUtil.hovered(mouseX, mouseY, x.getValue().floatValue(), y.getValue().floatValue(), width, height)) {
                 dragging = true;
                 dragX = mouseX - x.getValue().floatValue();
                 dragY = mouseY - y.getValue().floatValue();
@@ -95,29 +88,33 @@ public class TargetHUD extends Module {
         }
     }
 
-    private float barWidth = WIDTH;
-    private float ghostBarWidth = WIDTH;
-    private float targetWidth = WIDTH;
-    private float targetWidth2 = WIDTH;
+    private float barWidth = width;
+    private float ghostBarWidth = width;
+    private float targetWidth = width;
+    private float targetWidth2 = width;
+
     private Animation animation;
     private Animation ghostAnimation;
 
     private void draw() {
+        float x = this.x.getValue().floatValue();
+        float y = this.y.getValue().floatValue();
+
+        Color color = Vanta.instance.moduleStorage.getT(Theme.class).colors[0];
+        Color color2 = Vanta.instance.moduleStorage.getT(Theme.class).colors[1];
+
         switch (mode.getValue()) {
             case "Vanta":
-                Color color = Vanta.instance.moduleStorage.getT(Theme.class).colors[0];
-                Color color2 = Vanta.instance.moduleStorage.getT(Theme.class).colors[1];
+                width = 130;
+                height = 40;
 
-                float x = this.x.getValue().floatValue();
-                float y = this.y.getValue().floatValue();
-
-                RenderUtil.rectangle(x, y, WIDTH, HEIHT, BACKGROUND);
-                RenderUtil.player_head((EntityPlayer) localTarget, x, y, 36f);
+                RenderUtil.rectangle(x, y, width, height, BACKGROUND);
+                RenderUtil.renderHead((EntityPlayer) localTarget, x, y, 36f);
                 CFonts.SFPT_MEDIUM_20.drawStringWithShadow(localTarget.getName(), x + 38, y + 4, Color.WHITE);
                 CFonts.SFPT_REGULAR_18.drawStringWithShadow(String.format("%.1f", localTarget.getHealth()), x + 38, y + 15, Color.WHITE);
 
-                float healthWidth = WIDTH * (localTarget.getHealth() / localTarget.getMaxHealth());
-                float ghostWidth = WIDTH * (localTarget.getHealth() / localTarget.getMaxHealth());
+                float healthWidth = width * (localTarget.getHealth() / localTarget.getMaxHealth());
+                float ghostWidth = width * (localTarget.getHealth() / localTarget.getMaxHealth());
 
                 if (healthWidth != targetWidth) {
                     targetWidth = healthWidth;
@@ -159,38 +156,41 @@ public class TargetHUD extends Module {
                     ghostAnimation.start();
                 }
 
-                RenderUtil.rectangle(x, y + 36, WIDTH, 4f, bgbutdarker);
+                RenderUtil.rectangle(x, y + 36, width, 4f, DARKER_BACKGROUND);
                 RenderUtil.rectangle(x, y + 36, ghostBarWidth, 4f, color.darker());
-                RenderUtil.horizontal_grad(x, y + 36, barWidth, 4f, color2, color);
-
-                if (dragging && mc.currentScreen instanceof GuiChat) {
-                    RenderUtil.rectangle(x - 0.5, y - 0.5, WIDTH + 1, HEIHT + 1, false, color);
-                }
+                RenderUtil.rectangleGradientHorizontal(x, y + 36, barWidth, 4f, color2, color);
                 break;
 
             case "Classic":
-                float xval = this.x.getValue().floatValue();
-                float yval = this.y.getValue().floatValue();
-                float width = 150f;
-                float height = 55f;
+                width = 150f;
+                height = 55f;
+
                 float healthbarwidth = 100f;
                 float whatever = (localTarget.getHealth() / localTarget.getMaxHealth());
                 float healthbar = healthbarwidth * whatever;
+
                 String health_str = String.format("%.1f", localTarget.getHealth());
                 Color healthbarcol = new Color(48, 246, 6);
+
                 if (whatever < 0.5 && whatever > 0.33) {
                     healthbarcol = new Color(221, 244, 2);
                 }
+
                 if (whatever <= 0.33) {
                     healthbarcol = new Color(250, 42, 68);
                 }
-                RenderUtil.rectangle(xval, yval, width, height, true, BACKGROUND);
-                RenderUtil.RenderEntity((int) xval + 15,(int) yval + 52,25,-30,0,localTarget);
-                RenderUtil.rectangle(xval + 37, yval - (10f / 2) + (height / 2) + 10,healthbarwidth , 10f,bgbutdarker);
-                RenderUtil.rectangle(xval + 37, yval - (10f / 2) + (height / 2) + 10,healthbar , 10f,healthbarcol);
-                font.drawString(health_str + " ❤", xval + 37,yval + 16,Color.WHITE,true);
-                font.drawString(localTarget.getName(),xval + 37,yval + 2,Color.WHITE,true);
+
+                RenderUtil.rectangle(x, y, width, height, true, BACKGROUND);
+                RenderUtil.renderEntity((int) x + 15, (int) y + 52, 25, -30, 0, localTarget);
+                RenderUtil.rectangle(x + 37, y - (10f / 2) + (height / 2) + 10, healthbarwidth, 10f, DARKER_BACKGROUND);
+                RenderUtil.rectangle(x + 37, y - (10f / 2) + (height / 2) + 10, healthbar, 10f, healthbarcol);
+                mc.fontRendererObj.drawStringWithShadow(health_str + " ❤", x + 37, y + 16, Color.WHITE);
+                mc.fontRendererObj.drawStringWithShadow(localTarget.getName(), x + 37, y + 2, Color.WHITE);
                 break;
+        }
+
+        if (dragging && mc.currentScreen instanceof GuiChat) {
+            RenderUtil.rectangle(x - 0.5, y - 0.5, width + 1, height + 1, false, color);
         }
     }
 }
