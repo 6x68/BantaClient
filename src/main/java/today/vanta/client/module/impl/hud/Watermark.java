9facf6d18558db0b55797e995cb68e98e18d5a93
desync_lord <mark@wonderland.ac>
2026-06-13 14:49:58 +0300

    feat: adjust watermark

--- src/main/java/today/vanta/client/module/impl/hud/Watermark.java
+++ src/main/java/today/vanta/client/module/impl/hud/Watermark.java
@@ -7,7 +7,6 @@ import today.vanta.client.event.impl.game.render.Render2DEvent;
 import today.vanta.client.module.Category;
 import today.vanta.client.module.Module;
 import today.vanta.client.module.impl.client.Theme;
-import today.vanta.client.module.impl.hud.arraylist.BitMapRenderer;
 import today.vanta.client.setting.Setting;
 import today.vanta.client.setting.impl.BooleanSetting;
 import today.vanta.client.setting.impl.StringSetting;
@@ -23,9 +22,8 @@ import java.util.Calendar;
 import java.util.Formatter;
 
 public class Watermark extends Module {
-
-    private final StringSetting style = Setting.of("Style", "Vanta", "Vanta", "Compact", "Jello", "My Eyes", "Char", "Exhi"); //travis scott reference? // shut the fuck up!
-    private final BooleanSetting mcfont = Setting.of("Standard Minecraft Font", true).hide(() -> !style.getValue().equals("Exhi"));
+    private final StringSetting style = Setting.of("Style", "Vanta", "Vanta", "Compact", "Jello", "Char", "Exhi", "Adjust"); //travis scott reference? // shut the fuck up!
+    private final BooleanSetting mcfont = Setting.of("Vanilla font", true).hide(() -> !style.getValue().equals("Exhi"));
 
     public Watermark() {
         super("Watermark", "Draws a watermark of the client.", Category.HUD);
@@ -35,11 +33,10 @@ public class Watermark extends Module {
 
     @EventListen(priority = EventPriority.LOWEST)
     private void onRender(Render2DEvent event) {
-        BitMapRenderer exhiFont = new BitMapRenderer(mc.exhiFontRendererObj);
-        BitMapRenderer mcFont = new BitMapRenderer(mc.fontRendererObj);
-        String firstCharacter = String.valueOf(IClient.CLIENT_NAME.charAt(0));
-        float length = CFonts.SFPT_SEMIBOLD_42.getStringWidth(firstCharacter) - 1;
-        String watermarkfull = IClient.CLIENT_NAME.substring(1);
+        String firstChar = String.valueOf(IClient.CLIENT_NAME.charAt(0));
+        float firstCharWidth = CFonts.SFPT_SEMIBOLD_42.getStringWidth(firstChar) - 1;
+        String watermarkText = IClient.CLIENT_NAME.substring(1);
+
         Color[] colors = Vanta.instance.moduleStorage.getT(Theme.class).colors;
 
         float x = 5;
@@ -50,6 +47,7 @@ public class Watermark extends Module {
                 CFonts.SFPT_SEMIBOLD_42.drawStringWithShadow(IClient.CLIENT_NAME, x, y, colors[0]);
                 CFonts.SFPT_MEDIUM_18.drawStringWithShadow(IClient.CLIENT_VERSION, x, y + 18 + 3, Color.WHITE);
                 break;
+
             case "Compact":
                 String text = "§fV§rA§fNTA" + " | Steve | §r120§f FPS | mc.hypixel.net";
                 text = text.replace("Steve", mc.session.getUsername());
@@ -102,84 +100,36 @@ public class Watermark extends Module {
 
                 CFonts.SFPT_MEDIUM_18.drawString(text, textX, y + 2.5f, colors[0]);
                 break;
+
             case "Jello":
                 CFonts.HN_REGULAR_48.drawString(IClient.CLIENT_NAME, x, y, new Color(255, 255, 255, 185));
                 CFonts.HN_MEDIUM_24.drawString("Jello", x, y + CFonts.HN_REGULAR_48.getFontHeight() - 1, new Color(255, 255, 255, 185));
                 break;
-            case "My Eyes":
-                CFonts.RK_REGULAR_50.drawHorizontalGradientString("Abdelrahman Al-Rashid Client (Miniblox Bypass Edition)", x, y, colors[0], colors[1], 1, 150);
-                break;
+
             case "Char":
-                CFonts.SFPT_SEMIBOLD_42.drawStringWithShadow(firstCharacter, x, y, colors[0]);
-                CFonts.SFPT_SEMIBOLD_42.drawStringWithShadow(watermarkfull, x + length, y, Color.WHITE);
+                CFonts.SFPT_SEMIBOLD_42.drawStringWithShadow(firstChar, x, y, colors[0]);
+                CFonts.SFPT_SEMIBOLD_42.drawStringWithShadow(watermarkText, x + firstCharWidth, y, Color.WHITE);
                 break;
+
             case "Exhi":
                 Formatter format = new Formatter();
                 Calendar gfg_calender = Calendar.getInstance();
                 format.format("%tl:%tM %Tp", gfg_calender,
                         gfg_calender, gfg_calender);
+
                 if (mcfont.getValue()) {
-                    mcFont.drawString(firstCharacter, 2, 2, colors[0], true);
-                    mcFont.drawString(watermarkfull + EnumChatFormatting.GRAY + " [" + EnumChatFormatting.WHITE + format + EnumChatFormatting.GRAY + "] " + "[" + EnumChatFormatting.WHITE + mc.getDebugFPS() + " FPS" + EnumChatFormatting.GRAY + "]", 8, 2, Color.WHITE, true);
+                    mc.fontRendererObj.drawString(firstChar, 2, 2, colors[0], true);
+                    mc.fontRendererObj.drawString(watermarkText + EnumChatFormatting.GRAY + " [" + EnumChatFormatting.WHITE + format + EnumChatFormatting.GRAY + "] " + "[" + EnumChatFormatting.WHITE + mc.getDebugFPS() + " FPS" + EnumChatFormatting.GRAY + "]", 8, 2, Color.WHITE, true);
                 } else {
-                    exhiFont.drawString(firstCharacter, 2, 2, colors[0], true);
-                    exhiFont.drawString(watermarkfull + EnumChatFormatting.GRAY + " [" + EnumChatFormatting.WHITE + format + EnumChatFormatting.GRAY + "] " + "[" + EnumChatFormatting.WHITE + mc.getDebugFPS() + " FPS" + EnumChatFormatting.GRAY + "]", 9, 2, Color.WHITE, true);
-                    exhiFont.drawString("BPS: " + MovementUtil.getBPS(), 2, 12, Color.WHITE, true);
+                    mc.exhiFontRendererObj.drawString(firstChar, 2, 2, colors[0], true);
+                    mc.exhiFontRendererObj.drawString(watermarkText + EnumChatFormatting.GRAY + " [" + EnumChatFormatting.WHITE + format + EnumChatFormatting.GRAY + "] " + "[" + EnumChatFormatting.WHITE + mc.getDebugFPS() + " FPS" + EnumChatFormatting.GRAY + "]", 9, 2, Color.WHITE, true);
+                    mc.exhiFontRendererObj.drawString("BPS: " + MovementUtil.getBPS(), 2, 12, Color.WHITE, true);
                 }
+                break;
+
+            case "Adjust":
+                CFonts.SFPT_MEDIUM_20.drawStringWithShadow("§r§l" + firstChar + "§f" + watermarkText, 2, 2, colors[0]);
                 break;
         }
     }
-
-    //private static Color epstein = new Color(0, 0, 0, 255);
-    //private float tick;
-
-    /*
-    @EventListen
-    private void onUpdate(UpdateEvent event) {
-        tick++;
-
-        if (tick == 1) {
-            epstein = Color.WHITE;
-        }
-
-        if (tick == 3) {
-            epstein = Color.GREEN;
-        }
-
-        if (tick == 5) {
-            epstein = Color.PINK;
-        }
-
-        if (tick == 7) {
-            epstein = Color.CYAN;
-        }
-
-        if (tick == 10) {
-            epstein = Color.LIGHT_GRAY;
-        }
-
-        if (tick == 12) {
-            epstein = Color.BLACK;
-        }
-
-        if (tick == 14) {
-            epstein = Color.MAGENTA;
-        }
-        if (tick == 16) {
-            epstein = Color.ORANGE;
-        }
-
-        if (tick == 18) {
-            epstein = Color.RED;
-        }
-
-        if (tick == 20) {
-            epstein = Color.yellow;
-        }
-
-        if (tick == 22) {
-            tick = 1;
-        }
-    }
-     */
 }
