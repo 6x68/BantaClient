46ef7ef692b98459c272cbbcb0a96b01c419add6
desync_lord <mark@wonderland.ac>
2026-06-20 20:26:37 +0300

    chore: make changelog be final

--- src/main/java/today/vanta/util/client/IClient.java
+++ src/main/java/today/vanta/util/client/IClient.java
@@ -1,8 +1,13 @@
 package today.vanta.util.client;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface IClient {
     String CLIENT_NAME = "Vanta";
     String CLIENT_VERSION = "1.3";
     String DEVELOPERS = "made by mark & luna";
     String CLIENT_FULL_TITLE = CLIENT_NAME + " - " + CLIENT_VERSION + " - " + DEVELOPERS;
+
+    List<String> CHANGELOG = Arrays.asList("[+] Added 'Custom' speed mode", "[+] Added Jesus");
 }
