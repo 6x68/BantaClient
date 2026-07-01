9d6c4e6abd25fd813e1b0805a53f09c2da765f45
desync_lord <mark@wonderland.ac>
2026-07-01 22:27:05 +0300

    feat: open chat with command prefix key

--- src/main/java/today/vanta/client/processor/impl/KeyProcessor.java
+++ src/main/java/today/vanta/client/processor/impl/KeyProcessor.java
@@ -1,14 +1,20 @@
 package today.vanta.client.processor.impl;
 
+import net.minecraft.client.gui.GuiChat;
 import today.vanta.Vanta;
 import today.vanta.client.event.impl.system.KeyboardEvent;
 import today.vanta.client.processor.Processor;
+import today.vanta.util.client.IClient;
 import today.vanta.util.game.events.EventListen;
 import today.vanta.util.game.events.EventPriority;
 
 public class KeyProcessor extends Processor {
     @EventListen(priority = EventPriority.HIGHEST)
     private void onKey(KeyboardEvent event) {
+        if (event.key == IClient.COMMAND_PREFIX_KEY) {
+            mc.displayGuiScreen(new GuiChat(IClient.COMMAND_PREFIX));
+        }
+
         Vanta.instance.moduleStorage.list.forEach(mod -> {
             if (event.key == mod.key) {
                 mod.setEnabled(!mod.isEnabled());
--- src/main/java/today/vanta/util/client/IClient.java
+++ src/main/java/today/vanta/util/client/IClient.java
@@ -1,5 +1,7 @@
 package today.vanta.util.client;
 
+import org.lwjgl.input.Keyboard;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,6 +10,7 @@ public interface IClient {
     String CLIENT_VERSION = "1.4";
     String DEVELOPERS = "made by mark & luna";
     String CLIENT_FULL_TITLE = CLIENT_NAME + " - " + CLIENT_VERSION + " - " + DEVELOPERS;
+    int COMMAND_PREFIX_KEY = Keyboard.KEY_PERIOD;
     String COMMAND_PREFIX = ".";
 
     List<String> CHANGELOG = Arrays.asList(
