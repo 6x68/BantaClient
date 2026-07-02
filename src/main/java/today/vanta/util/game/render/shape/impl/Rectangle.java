f260296bb4bd3a0cd59ffd3363bf2c6bb512acdb
desync_lord <mark@wonderland.ac>
2026-07-02 13:07:15 +0300

    chore: correct more render calls

--- src/main/java/today/vanta/util/game/render/shape/impl/Rectangle.java
+++ src/main/java/today/vanta/util/game/render/shape/impl/Rectangle.java
@@ -7,8 +7,6 @@ import today.vanta.util.game.render.shape.Shape;
 import javax.annotation.Nonnull;
 import java.awt.*;
 
-import static org.lwjgl.opengl.GL11.*;
-
 public class Rectangle extends Shape<Rectangle> {
     private Color color = Color.WHITE;
 
@@ -53,7 +51,7 @@ public class Rectangle extends Shape<Rectangle> {
         GL11.glLineWidth(outlineWidth);
 
         RenderUtil.color(color);
-        GL11.glBegin(outline ? GL_LINE_LOOP : GL_QUADS);
+        GL11.glBegin(outline ? GL11.GL_LINE_LOOP : GL11.GL_QUADS);
         GL11.glVertex2d(x, y);
         GL11.glVertex2d(x + width, y);
         GL11.glVertex2d(x + width, y + height);
