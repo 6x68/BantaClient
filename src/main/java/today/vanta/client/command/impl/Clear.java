2498c423bbad4ee1583bc6245bf1899e3b213219
desync_lord <mark@wonderland.ac>
2026-07-03 20:02:08 +0300

    Update Clear.java

--- src/main/java/today/vanta/client/command/impl/Clear.java
+++ src/main/java/today/vanta/client/command/impl/Clear.java
@@ -5,7 +5,7 @@ import today.vanta.client.command.Command;
 public class Clear extends Command {
     public Clear() {
         super("Clear", "Clears chat.");
-        aliases = new String[]{"clear", "cc", "c"};
+        aliases = new String[]{"clear", "cc", "cls"};
     }
 
     @Override
