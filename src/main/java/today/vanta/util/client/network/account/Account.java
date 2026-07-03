85b4f3a2e84d3e6cea6f4b31b45e2a7e36d377ca
desync_lord <mark@wonderland.ac>
2026-07-03 20:02:01 +0300

    chore: remove old config system

--- src/main/java/today/vanta/util/client/network/account/Account.java
+++ src/main/java/today/vanta/util/client/network/account/Account.java
@@ -3,7 +3,7 @@ package today.vanta.util.client.network.account;
 public class Account {
     public String username, uuid;
     public String token = "", refreshToken = "";
-    public String skin = AccountSavingUtil.getSteveHead();
+    public String skin = getSteveHead();
 
     public Account(String username, String uuid) {
         this.username = username;
@@ -21,11 +21,11 @@ public class Account {
         this.refreshToken = refreshToken;
     }
 
-    public boolean isEmail() {
-        return username.contains("@");
-    }
-
     public boolean isCracked() {
         return uuid.isEmpty();
+    }
+
+    private static String getSteveHead() {
+        return "iVBORw0KGgoAAAANSUhEUgAAASwAAAEsCAIAAAD2HxkiAAAE1UlEQVR4nOzVu4rdZRuH4XcyazL58uGmylgIsbdQ+5BS3JTaWwS08BBEW0WxsRHEytIgFlqkSpFaC1uFuCGoKUIgRMMks2ZkWnufu7muE/it/wv3ejbPPfPYGnR0PLl2arNzZnLucHs0OXf6pEfbybkzZ0bfc621t9mdnDs5mVw7Nf2gwL+IEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCG2OTquf8J/7Ohk9Av/f+7s5Nxa64mzO5Nz27U7ObfWunP/cHJue7KdnHMJoSdCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIIbYZ3vvo7TeGF/f3zk7O/e/c45Nza60Hf90b3dud/uO+++fvk3MffH1tcs4lhJ4IISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGI7X7775uTe+f39ybm11t+Hh5Nzu7vTH3j33t3JuYsHFybn1lq//vLj8OIwlxBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBim/P7+5N7r77zyeTcWuu1yx9Ozl25fHNybq118eDC5Nyn3x5Mzq21vrrx8eTc1ffempxzCaEnQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoTYzudXXpzcO3jq6cm5tdZm/8nJuW+uX5+cm/f6S5eGF2/99tPk3M3bdybnXELoiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIbe4/2k7uPbx1c3JurXX1xneTcy8/+8Lk3Lz3P/tiePGVS89Pzu2svck5lxB6IoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIbX7+4/bk3vHx8eTcvGs/fD+8uLu3GV4cdv/BdnLu8NHDyTmXEHoihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGIihJgIISZCiIkQYiKEmAghJkKIiRBiIoSYCCEmQoiJEGL/BAAA//+J4VhDvV4pYgAAAABJRU5ErkJggg==";
     }
 }
