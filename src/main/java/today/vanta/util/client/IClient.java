806ad841cd4169859776fd8101880ed5185a878b
desync_lord <mark@wonderland.ac>
2026-07-03 21:40:22 +0300

    chore: bump version

--- src/main/java/today/vanta/util/client/IClient.java
+++ src/main/java/today/vanta/util/client/IClient.java
@@ -7,28 +7,13 @@ import java.util.List;
 
 public interface IClient {
     String CLIENT_NAME = "Vanta";
-    String CLIENT_VERSION = "1.4";
+    String CLIENT_VERSION = "1.5";
     String DEVELOPERS = "made by mark & luna";
     String CLIENT_FULL_TITLE = CLIENT_NAME + " - " + CLIENT_VERSION + " - " + DEVELOPERS;
     int COMMAND_PREFIX_KEY = Keyboard.KEY_PERIOD;
     String COMMAND_PREFIX = ".";
 
     List<String> CHANGELOG = Arrays.asList(
-            "[+] Added Commands - help, bind, toggle, clear, teleport, config",
-            "[+] Added command suggestions",
-            "[+] Added Miniblox disabler",
-            "[+] Added more targethuds",
-            "[+] Added Collision Jesus",
-            "[+] Added refresh tokens",
-            "[+] Added ClickTeleport",
-            "[+] Added more themes",
-            "[+] Added AntiExploit",
-            "[+] Added AntiCheat",
-            "[+] Added Jump Fly",
-            "[#] Reworked InventoryManager",
-            "[#] Reworked ChestStealer",
-            "[#] Reworked Scoreboard",
-            "[#] Reworked configs",
-            "[#] Fixed rendering bugs"
+
     );
 }
