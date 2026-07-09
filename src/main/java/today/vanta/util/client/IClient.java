ddeb207eabcfecee972cd557617eb743ac39278a
desync_lord <mark@wonderland.ac>
2026-07-09 15:55:31 +0300

    chore: bump version

--- src/main/java/today/vanta/util/client/IClient.java
+++ src/main/java/today/vanta/util/client/IClient.java
@@ -7,28 +7,13 @@ import java.util.List;
 
 public interface IClient {
     String CLIENT_NAME = "Vanta";
-    String CLIENT_VERSION = "1.5";
+    String CLIENT_VERSION = "1.6";
     String DEVELOPERS = "made by mark & luna";
     String CLIENT_FULL_TITLE = CLIENT_NAME + " - " + CLIENT_VERSION + " - " + DEVELOPERS;
     int COMMAND_PREFIX_KEY = Keyboard.KEY_PERIOD;
     String COMMAND_PREFIX = ".";
 
     List<String> CHANGELOG = Arrays.asList(
-            "[#] Made ClickGui togglable in Main Menu",
-            "[#] ClickTeleport now draws destination",
-            "[#] Fixed ClientSounds",
-            "[#] Fixed Scaffold",
-            "[+] Added Clicking settings module",
-            "[+] Added session stat modules",
-            "[+] Added Arraylist animations",
-            "[+] Added new Boxy ClickGUI",
-            "[+] Added WindowSettings",
-            "[+] Added AutoRegister",
-            "[+] Added Image ESP",
-            "[+] Added AutoPlay",
-            "[+] Added AntiVoid",
-            "[+] Added NoFall",
-            "[+] Added Arrows",
-            "[+] Added VClip"
+
     );
 }
